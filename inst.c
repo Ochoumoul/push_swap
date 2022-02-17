e849e7225d52a3b169de5fc9c8c45d9a1cd0baf0
Oussama Choumoul <choumouloussama@gmail.com>
2022-02-17 18:21:50 +0100

    Completed instructions

--- inst.c
+++ inst.c
@@ -12,67 +12,111 @@ typedef struct node {
 
 typedef t_node *stack;
 
-void    ft_print_inst(char inst,char s)
+void    ft_print_inst(char *inst,char s)
 {
-    ft_putchar(inst);
+    ft_putstr(inst);
     ft_putchar(s);
     ft_putchar('\n');
 }
 
-int    push_el(stack *stack, int value, char s)
+int    push_el(stack *stack, int value)
 {
     t_node *node;
 
-    ft_print_inst('p',s);
-    // The function above will be responsible for printing the instruction.
     node = malloc(sizeof(t_node));
     if (!node)
         return (0);
     node->value = value;
     node->next = *stack;
     *stack = node;
-    return (1); // 1 means that element have been pushed succesfully.
+    return (1);
 }
 
 int     pop_el(stack *stack)
 {
     if (*stack == NULL) return STACK_EMPTY;
 
-    t_node *tmp = *stack;
-    int result = (*stack)->value;
+    t_node *tmp;
+    int result;
+    
+    tmp = *stack;
+    result = (*stack)->value;
     *stack = (*stack)->next;
     free(tmp);
     return (result);
 }
 
-// The double swap function like ss should take 2 stacks as an argument.
-
-void    swap_el(stack *stack, char s)
+int   push_stack(stack *stackA, stack *stackB)
 {
-    int tmp;
-    
-    ft_print_inst('s',s);
-    // The function above will be responsible for printing the instruction.
-    tmp = (*stack)->value;
-    (*stack)->value = (*stack)->next->value;
-    (*stack)->next->value = tmp;
+    int value;
+
+    value = pop_el(stackA);
+    return (push_el(stackB, value));
 }
 
-int    rotate_el(stack *stack, char s)
+int    rotate_el(stack *stack)
 {
     t_node *node;
+    t_node *tmp_stack;
 
-    ft_print_inst('r',s);
-    // The function above will be responsible for printing the instruction.
+    tmp_stack = *stack;
+
     node = malloc(sizeof(t_node));
     if (!node)
         return (0);
     node->value = pop_el(stack);
     node->next = NULL;
-    while (*stack != NULL)
-        *stack = (*stack)->next;
+    while (tmp_stack->next != NULL)
+        tmp_stack = tmp_stack->next;
+    tmp_stack->next = node;
+    return (1);
+}
+
+int     reverse_rotate_el(stack *stack)
+{
+    t_node *node;
+    t_node *tmp;
+
+    tmp = *stack;
+
+    node = malloc(sizeof(t_node));
+    if (!node)
+        return (0);
+    while (tmp->next->next != NULL)
+        tmp = tmp->next;
+    node->value = tmp->next->value;
+    node->next = *stack;
+    tmp->next = NULL;
+
     *stack = node;
     return (1);
+}
+
+void    swap_el(stack *stack)
+{
+    int tmp;
+    
+    tmp = (*stack)->value;
+    (*stack)->value = (*stack)->next->value;
+    (*stack)->next->value = tmp;
+}
+
+void swap_both(stack *stackA, stack *stackB)
+{
+    swap_el(stackA);
+    swap_el(stackB);
+}
+
+void    rotate_both(stack *stackA, stack *stackB)
+{
+    rotate_el(stackA);
+    rotate_el(stackB);
+}
+
+void    reverse_rotate_both(stack *stackA, stack *stackB)
+{
+    reverse_rotate_el(stackA);
+    reverse_rotate_el(stackB);
 }
 
 int main(void)
@@ -83,20 +127,14 @@ int main(void)
     a = NULL;
     b = NULL;
 
-    push_el(&a, 5, 'a');
-    push_el(&a, 17, 'a');
-    push_el(&a, 45, 'a');
-    push_el(&a, 51, 'a');
-    push_el(&a, 12, 'a');
-    push_el(&a, 19, 'a');
-    rotate_el(&a, 'a');
-    push_el(&b, 51, 'b');
+    push_el(&a, 3);
+    push_el(&a, 9);
+    push_el(&a, 7);
+    push_el(&a, 6);
+    reverse_rotate_el(&a);
 
     printf("===========Stack A=========\n");
     while ((result = pop_el(&a)) != STACK_EMPTY)
-        printf("%d\n", result);
-    printf("===========Stack B=========\n");
-    while ((result = pop_el(&b)) != STACK_EMPTY)
         printf("%d\n", result);
     return (0);
 }
